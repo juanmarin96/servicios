68ae529903a2c1b18e45bf6a504d663a0c73b6a3
juan carlos gonzales montes <pope624@hotmail.com>
2017-06-11 14:51:02 -0500

    menu fijo

---
+++ Servicios2/Servicios2/wwwroot/js/menu.js
@@ -0,0 +1,30 @@
+﻿/*$(document).ready(function () {
+    var altura = $('.menu').offset().top;
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > altura) {
+            
+            $('.menu').addClass('navbar-fixed-top');
+        } else {
+            $('.menu').removeClass('navbar-fixed-top');
+        }
+    });
+
+});*/
+
+posicionarMenu();
+
+$(window).scroll(function () {
+    posicionarMenu();
+});
+
+function posicionarMenu() {
+    var altura_del_header = $('.Cimagen').outerHeight(true);
+    var altura_del_menu = $('.menu').outerHeight(true);
+
+    if ($(window).scrollTop() >= altura_del_header) {
+        $('.menu').addClass('menu-fixed');
+    } else {
+        $('.menu').removeClass('menu-fixed');
+    }
+}
