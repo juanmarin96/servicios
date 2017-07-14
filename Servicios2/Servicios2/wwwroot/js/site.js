bd99b457ffda5d5eadacfa4675fcf0351a49db04
Camilo Lopez <camilopezc@hotmail.com>
2017-07-13 22:27:46 -0500

    Home Full 1

--- Servicios2/Servicios2/wwwroot/js/site.js
+++ Servicios2/Servicios2/wwwroot/js/site.js
@@ -11,38 +11,50 @@ function changebg() {
     if (img == 0) {
         $('.img-estrategia').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Estrategia");
-        $('#titulo-encabezado').html("Estrategia");
+        $('#titulo-encabezado').html("Estrategia  ");
         $('.titulo-encab').css('color', '#ff6d40');
+        $('.flecha-servicios').css('background', '#ff6d40');
+        $('.flecha-servicios-2').css('background', '#ff6d40');
     }        
     if (img == 1) {
         $('.img-innovacion').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Innovacion");
-        $('#titulo-encabezado').html("Innovacion");
+        $('#titulo-encabezado').html("Innovacion  ");
         $('.titulo-encab').css('color', '#ffab40');
+        $('.flecha-servicios').css('background', '#ffab40');
+        $('.flecha-servicios-2').css('background', '#ffab40');
     }   
     if (img == 2) {
         $('.img-neuromarketing').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Neuromarketing");
-        $('#titulo-encabezado').html("Neuromarketing");
+        $('#titulo-encabezado').html("Neuromarketing  ");
         $('.titulo-encab').css('color', '#ffea00');
+        $('.flecha-servicios').css('background', '#ffea00');
+        $('.flecha-servicios-2').css('background', '#ffea00');
     }
     if (img == 3) {
         $('.img-cmr').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Cmr");
-        $('#titulo-encabezado').html("Cmr");
+        $('#titulo-encabezado').html("Cmr  ");
         $('.titulo-encab').css('color', '#b1ff58');
+        $('.flecha-servicios').css('background', '#b1ff58');
+        $('.flecha-servicios-2').css('background', '#b1ff58');
     }
     if (img == 4) {
         $('.img-shopper').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Shopper");
-        $('#titulo-encabezado').html("Shopper");
+        $('#titulo-encabezado').html("Shopper  ");
         $('.titulo-encab').css('color', '#40c4ff');
+        $('.flecha-servicios').css('background', '#40c4ff');
+        $('.flecha-servicios-2').css('background', '#40c4ff');
     }
     if (img == 5) {
         $('.img-marketing-mix').css('filter', 'grayscale(0%)');
         $('#texto-encabezado').html("Aqui va texto de Marketing-mix");
-        $('#titulo-encabezado').html("Marketing-mix");
+        $('#titulo-encabezado').html("Marketing-mix  ");
         $('.titulo-encab').css('color', '#7b4dff');
+        $('.flecha-servicios').css('background', '#7b4dff');
+        $('.flecha-servicios-2').css('background', '#7b4dff');
     }     
     img = (img + 1) % 6;
     setTimeout(changebg, 10000);
@@ -51,8 +63,10 @@ function changebg() {
 $(function () {
     $('.img-innovacion').css('filter', 'grayscale(0%)');
     $('#texto-encabezado').html("Aqui va texto de Innovacion");
-    $('#titulo-encabezado').html("Innovacion");
+    $('#titulo-encabezado').html("Innovacion  ");
     $('.titulo-encab').css('color', '#ffab40');
+    $('.flecha-servicios').css('background', '#ffab40');
+    $('.flecha-servicios-2').css('background', '#ffab40');
     setTimeout(changebg, 10000);
 })
 
